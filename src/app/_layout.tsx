4b9dc841097ef459903e87f40c90998cd07150dd
Alan Acharya <alanacharya1@gmail.com>
2026-06-01 20:51:20 +0545

    fix layout theme switching with dynamic GestureHandlerRootView background

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,31 +1,30 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "../../global.css";
-import { colors } from "../constants/colors";
 import { FavoritesProvider } from "../context/FavoritesContext";
 import { SettingsProvider, useSettings } from "../context/SettingsContext";
 
-function RootStack() {
+function AppContent() {
   const { currentColors } = useSettings();
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: currentColors.background },
-        animation: "fade",
-      }}
-    />
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: currentColors.background }}>
+      <FavoritesProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: currentColors.background },
+            animation: "fade",
+          }}
+        />
+      </FavoritesProvider>
+    </GestureHandlerRootView>
   );
 }
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1, backgroundColor: colors.background }}>
-      <SettingsProvider>
-        <FavoritesProvider>
-          <RootStack />
-        </FavoritesProvider>
-      </SettingsProvider>
-    </GestureHandlerRootView>
+    <SettingsProvider>
+      <AppContent />
+    </SettingsProvider>
   );
 }
